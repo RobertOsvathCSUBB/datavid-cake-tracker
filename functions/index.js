706b180a51eb3364e9bb9eb0588f62143a7924d2
RobertOsvathCSUBB <robert.osvath@stud.ubbcluj.ro>
2024-06-20 19:25:33 +0300

    done

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {setGlobalOptions} = require("firebase-functions/v2");
 setGlobalOptions({maxInstances: 10});
 
@@ -63,11 +54,3 @@ exports.validateAgeConstraint = onDocumentCreated("Members/{id}",
   }
   return null;
 });
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
